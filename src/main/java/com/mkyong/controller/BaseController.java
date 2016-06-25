c54696de44b03579cb728bb210505d262beda2fd
ivarun57 <ivarun57@gmail.com>
2016-06-25 15:39:04 +0530

    Adding mutiple comments for multiple files (#2)

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+// Class BaseController
+
 @Controller
 public class BaseController {
 
